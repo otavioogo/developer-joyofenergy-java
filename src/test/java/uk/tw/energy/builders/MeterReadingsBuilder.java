13e69cfe188db1428d92705c512c8e351c38b818
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-06-07 09:08:36 -0300

    Update MeterReadingsBuilder.java
    Organizando a classe Electricity

--- src/test/java/uk/tw/energy/builders/MeterReadingsBuilder.java
+++ src/test/java/uk/tw/energy/builders/MeterReadingsBuilder.java
@@ -1,8 +1,8 @@
 package uk.tw.energy.builders;
 
-import uk.tw.energy.domain.ElectricityReading;
+import uk.tw.energy.electricityReading.ElectricityReading;
 import uk.tw.energy.meterReading.MeterReadings;
-import uk.tw.energy.generator.ElectricityReadingsGenerator;
+import uk.tw.energy.electricityReading.ElectricityReadingsGenerator;
 
 import java.util.ArrayList;
 import java.util.List;

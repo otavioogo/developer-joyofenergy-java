6c77d7e10ce5364f95b81c692f3e79df93a06775
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-06-07 08:48:44 -0300

    Update PricePlanComparatorController.java

--- src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
+++ src/main/java/uk/tw/energy/controller/PricePlanComparatorController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uk.tw.energy.service.AccountService;
-import uk.tw.energy.service.PricePlanService;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;

1beef6b4b19f9f48a78550fb8f86b8b5df7955d0
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-06-07 09:10:18 -0300

    Update MeterReadingServiceTest.java
    Organizando a classe Electricity

--- src/test/java/uk/tw/energy/service/MeterReadingServiceTest.java
+++ src/test/java/uk/tw/energy/service/MeterReadingServiceTest.java
@@ -2,7 +2,7 @@ package uk.tw.energy.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import uk.tw.energy.electricityReading.MeterReadingService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;

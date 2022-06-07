a6fb6e498fdcb0e094d58d1a837e61ac3ac5ab69
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-06-07 08:51:27 -0300

    Update PricePlanComparatorControllerTest.java
    Organizando o pricePlan

--- src/test/java/uk/tw/energy/controller/PricePlanComparatorControllerTest.java
+++ src/test/java/uk/tw/energy/controller/PricePlanComparatorControllerTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import uk.tw.energy.domain.ElectricityReading;
-import uk.tw.energy.domain.PricePlan;
-import uk.tw.energy.service.AccountService;
+import uk.tw.energy.pricePlan.PricePlan;
+import uk.tw.energy.pricePlan.AccountService;
 import uk.tw.energy.service.MeterReadingService;
-import uk.tw.energy.service.PricePlanService;
+import uk.tw.energy.pricePlan.PricePlanComparatorController;
+import uk.tw.energy.pricePlan.PricePlanService;
 
 import java.math.BigDecimal;
 import java.time.Instant;
